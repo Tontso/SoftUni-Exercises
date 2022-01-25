eebba18849c8ab56c2980ba768250b62af48b2b5
Tontso <tommysketo977@gmail.com>
2022-01-26 00:51:41 +0200

    change directory

--- JS Advanced/1.Objects and Composition/calorie-object.js
+++
@@ -1,9 +0,0 @@
-function solve(input){
-    let result = {};
-
-    for (let i = 0; i < input.length; i+=2) {
-      result[input[i]] = Number(input[i+1]);
-    }
-    return result;
-}
-console.log(solve(['Yoghurt', '48', 'Rise', '138', 'Apple', '52']));
